fdde348667d2ba448fdfffbb7aec888702c0ff36
Luke Kozak <kozakluke@gmail.com>
2018-01-30 09:58:04 +0100

    Update Content, render columns

--- src/components/content/Content.css.d.ts
+++ src/components/content/Content.css.d.ts
@@ -2,10 +2,4 @@
  * @author kozakluke@gmail.com
  */
 export const content:string;
-export const element1:string;
-export const element2:string;
-export const element3:string;
-export const element4:string;
-export const element5:string;
-export const element6:string;
-export const element7:string;
+export const element:string;
--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -5,17 +5,60 @@ import * as style from './Content.css';
  */
 export default class Content extends React.PureComponent
 {
+    private images = [
+        'assets/projects/thumbs/banner1.jpg',
+        'assets/projects/thumbs/banner2.jpg',
+        'assets/projects/thumbs/banner3.jpg',
+        'assets/projects/thumbs/banner4.jpg',
+        'assets/projects/thumbs/banner5.jpg',
+        'assets/projects/thumbs/banner6.jpg',
+        'assets/projects/thumbs/banner7.jpg',
+        'assets/projects/thumbs/banner8.jpg',
+        'assets/projects/thumbs/banner9.jpg',
+        'assets/projects/thumbs/banner10.jpg',
+        'assets/projects/thumbs/banner11.jpg',
+        'assets/projects/thumbs/banner12.jpg',
+        'assets/projects/thumbs/banner13.jpg',
+        'assets/projects/thumbs/banner14.jpg',
+        'assets/projects/thumbs/banner15.jpg',
+        
+        'assets/projects/thumbs/info1.jpg',
+        'assets/projects/thumbs/info2.jpg',
+        
+        'assets/projects/thumbs/moto.jpg',
+        'assets/projects/thumbs/onion.jpg',
+        
+        'assets/projects/thumbs/play1.jpg',
+        'assets/projects/thumbs/play2.jpg',
+        
+        'assets/projects/thumbs/poker1.jpg',
+        'assets/projects/thumbs/poker2.jpg',
+        
+        'assets/projects/thumbs/shu1.jpg',
+        'assets/projects/thumbs/shu2.jpg',
+        'assets/projects/thumbs/shu3.jpg',
+        'assets/projects/thumbs/sign.jpg',
+        'assets/projects/thumbs/slot1.jpg',
+        'assets/projects/thumbs/slot2.jpg',
+        'assets/projects/thumbs/splash1.jpg',
+        'assets/projects/thumbs/splash2.jpg',
+        'assets/projects/thumbs/splash3.jpg',
+        'assets/projects/thumbs/UE.jpg',
+    ];
+    
     render()
     {
         return (
             <div className={style.content}>
-                <div className={style.element1}>1</div>
-                <div className={style.element2}>2</div>
-                <div className={style.element3}>3</div>
-                <div className={style.element4}>4</div>
-                <div className={style.element5}>5</div>
-                <div className={style.element6}>6</div>
-                <div className={style.element7}>7</div>
+                {this.images.map(
+                    (n:string, id:number)=>
+                        <div className={style.element} key={n}>
+                            <img  src={n}>
+                            
+                        </img>
+                        </div>
+                        
+                )}
             </div>
         );
     }
