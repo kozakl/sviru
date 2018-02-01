c43926c743521459cca65e88631ebf66a4a49a86
Luke Kozak <kozakluke@gmail.com>
2018-02-01 20:50:02 +0100

    Update Header, name to uppercase

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,10 +3,11 @@ import * as style from './Header.css';
 /**
  * @author kozakluke@gmail.com
  */
-export default function Header() {
+export default function Header()
+{
     return (
         <div className={style.header}>
-            <h1 className={style.name}>Tomasz Swirzewski</h1>
+            <h1 className={style.name}>TOMASZ SWIŻEWSKI</h1>
             <div className={style.phone}>
                 <span>&#xe901;</span>
                 <label>+48 503 995 015</label>
